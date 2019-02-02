bb07ee2b7b855ebba7a2f3589bce8e4fbe81b4f1
soso7df <44973092+soso7df@users.noreply.github.com>
2019-02-02 12:29:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Author: Sorabh Sodhani
+Date: 1st February, 2019
+Towards: Pirple.com's Python Is Easy
+Project: Homework #1 (Variables)
+
+Task: Assign and Print Variables
+"""
+
+
+# In[ ]:
+
+
+#Assigning Values to Variables
+Title = "La Vie en rose" #the title of the track
+Artist = "Louis Armstrong" #the main artist of the song, sometimes also the artist of the entire album
+Album = "None (Solo Recording)" #the album to which the song belongs
+Genre = "Jazz" #the category of the song
+Length = 3.23 #length of the song in minutes.seconds
+Track_Number = 1 #position of the song in the album, relative to other songs
+Bitrate_Unit = "kbps" #kilobytes per second
+Bitrate_Value = "320" #normally a value between 128 and 320, usually higher bitrates mean better sound quality
+Recording_Year = 1950 #year in which the song or album was recorded
+
+
+# In[ ]:
+
+
+#Printing Values of Variables 
+print ("Title:" ,Title)
+print ("Artist:", Artist)
+print ("Album:", Album)
+print ("Recorded in:", Recording_Year)
+print ("Genre:", Genre)
+print ("Length:", Length, "m")
+print ("Track Number:", Track_Number)
+print ("Bitrate:", Bitrate_Value, Bitrate_Unit)
+
