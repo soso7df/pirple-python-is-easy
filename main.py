1cb5087f69957d5aca59bd1ba98d646c7b98f79c
soso7df <44973092+soso7df@users.noreply.github.com>
2019-02-06 12:33:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,41 +4,63 @@
 # In[ ]:
 
 
-"""
-Author: Sorabh Sodhani
-Date: 1st February, 2019
-Towards: Pirple.com's Python Is Easy
-Project: Homework #1 (Variables)
+#Author: Sorabh Sodhani
+#Date: 6th February, 2019
+#Towards: Pirple.com's Python Is Easy
+#Project: Homework #5 (Loops)
 
-Task: Assign and Print Variables
-"""
+#Task: Use Loops to print words "Fizz" and "Buzz" for numbers between 1 and 100 divisible by 3 and 5 respectively along with an added functionality of printing "Prime" for prime numbers
 
 
 # In[ ]:
 
 
-#Assigning Values to Variables
-Title = "La Vie en rose" #the title of the track
-Artist = "Louis Armstrong" #the main artist of the song, sometimes also the artist of the entire album
-Album = "None (Solo Recording)" #the album to which the song belongs
-Genre = "Jazz" #the category of the song
-Length = 3.23 #length of the song in minutes.seconds
-Track_Number = 1 #position of the song in the album, relative to other songs
-Bitrate_Unit = "kbps" #kilobytes per second
-Bitrate_Value = "320" #normally a value between 128 and 320, usually higher bitrates mean better sound quality
-Recording_Year = 1950 #year in which the song or album was recorded
+# Important: Confusion (and Assumption Made)
+
+# What should be done for the special case of 3 and 5?
+# Because while they should make me print Fizz and Buzz respectively
+# But both numbers are also prime themselves
+
+# So I am printing both Fizz and Prime for 3
+# And Buzz and Prime for 5
 
 
 # In[ ]:
 
 
-#Printing Values of Variables 
-print ("Title:" ,Title)
-print ("Artist:", Artist)
-print ("Album:", Album)
-print ("Recorded in:", Recording_Year)
-print ("Genre:", Genre)
-print ("Length:", Length, "m")
-print ("Track Number:", Track_Number)
-print ("Bitrate:", Bitrate_Value, Bitrate_Unit)
+# Defining a function to deduct if a given number is a prime number or not
+
+def isPrime(N):
+    for n in range(2, N): # because we need a range from 2 to (N - 1) 
+        if (N % n == 0):  # N is divisible by at least 1 number between 2 and (N-1)
+            return False  # return False if not a prime
+
+    return True
+
+
+# In[ ]:
+
+
+num1, num2 = 3, 5 #using variables to generalise the code
+
+# Loop through all numbers from 1 to 100
+for n in range(1,101):
+    
+    # Initialise a string that we will eventually print
+    toPrint = ""
+    
+    # Check if current number is divisible by  3
+    if (n % num1 == 0):
+        toPrint = "Fizz"
+    
+    # Check if current number is divisible by 5
+    if (n % num2 == 0):
+        toPrint += "Buzz"
+       
+    # Check if current number is a prime
+    if (isPrime(n)):
+        toPrint += "Prime"
+        
+    # Print the number, and the results from the three checks above
+    print(n, toPrint)
 
