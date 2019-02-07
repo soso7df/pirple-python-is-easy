8c1be9d7657b480f550b4a4739d847e854391b03
soso7df <44973092+soso7df@users.noreply.github.com>
2019-02-07 12:37:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,41 +4,59 @@
 # In[ ]:
 
 
-"""
-Author: Sorabh Sodhani
-Date: 1st February, 2019
-Towards: Pirple.com's Python Is Easy
-Project: Homework #1 (Variables)
+#Author: Sorabh Sodhani
+#Date: 7th February, 2019
+#Towards: Pirple.com's Python Is Easy
+#Project: Homework #6 (Advanced Loops)
 
-Task: Assign and Print Variables
-"""
+#Task: Use functions and loops to print a grid-board with a required number of rows and columns
 
 
 # In[ ]:
 
 
-#Assigning Values to Variables
-Title = "La Vie en rose" #the title of the track
-Artist = "Louis Armstrong" #the main artist of the song, sometimes also the artist of the entire album
-Album = "None (Solo Recording)" #the album to which the song belongs
-Genre = "Jazz" #the category of the song
-Length = 3.23 #length of the song in minutes.seconds
-Track_Number = 1 #position of the song in the album, relative to other songs
-Bitrate_Unit = "kbps" #kilobytes per second
-Bitrate_Value = "320" #normally a value between 128 and 320, usually higher bitrates mean better sound quality
-Recording_Year = 1950 #year in which the song or album was recorded
+## Setting two global variables 
+# for the maximum width and maximum height that my terminal can take 
+# This was achieved by trial and error
+    
+max_rows, max_columns = 22, 22
 
 
 # In[ ]:
 
 
-#Printing Values of Variables 
-print ("Title:" ,Title)
-print ("Artist:", Artist)
-print ("Album:", Album)
-print ("Recorded in:", Recording_Year)
-print ("Genre:", Genre)
-print ("Length:", Length, "m")
-print ("Track Number:", Track_Number)
-print ("Bitrate:", Bitrate_Value, Bitrate_Unit)
+# Defining the function which takes two inputs - rows, and columns
+# and draws a grid-like board with as many rows and columns as requested
+
+def drawBoard(rows, columns):
+    # If either the requested number of rows and columns is greater than the allowed number
+    # then return False
+    
+    if (rows > max_rows) or (columns > max_columns):
+        return False
+    
+    
+    for r in range (1, 2*rows):
+
+        if( r%2 == 0):
+            horizontal_separator = "-" + "--+--" * (columns-1) + "-"
+            print (horizontal_separator)
+
+        else:
+            vertical_separator = " " + "  |  " * (columns - 1) + " "
+            print (vertical_separator)
+
+    return True
+
+
+# In[ ]:
+
+
+# Calling the function to draw the board
+
+rows, columns = 5, 7
+if (drawBoard(rows,columns) == False):
+    print ("Oops! Either number of rows or number of columns exceeds the limits set as below")
+    print ("Maximum rows: ", max_rows)
+    print ("Maximum columns: ", max_columns)
 
