2a419f98ec4f53e4c1fa8ab59c2f2e4a1ee787bd
soso7df <44973092+soso7df@users.noreply.github.com>
2019-02-19 16:37:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,41 +4,120 @@
 # In[ ]:
 
 
-"""
-Author: Sorabh Sodhani
-Date: 1st February, 2019
-Towards: Pirple.com's Python Is Easy
-Project: Homework #1 (Variables)
+# Author: Sorabh Sodhani
+# Date: 19th February, 2019
+# Towards: Pirple.com's Python Is Easy
+# Project: Homework #8 (I/O)
 
-Task: Assign and Print Variables
-"""
+#Task: File Operations
 
 
-# In[ ]:
+# In[2]:
 
 
-#Assigning Values to Variables
-Title = "La Vie en rose" #the title of the track
-Artist = "Louis Armstrong" #the main artist of the song, sometimes also the artist of the entire album
-Album = "None (Solo Recording)" #the album to which the song belongs
-Genre = "Jazz" #the category of the song
-Length = 3.23 #length of the song in minutes.seconds
-Track_Number = 1 #position of the song in the album, relative to other songs
-Bitrate_Unit = "kbps" #kilobytes per second
-Bitrate_Value = "320" #normally a value between 128 and 320, usually higher bitrates mean better sound quality
-Recording_Year = 1950 #year in which the song or album was recorded
+# Querying user for the file name 
+
+fileName = str(input("Please enter the name of the file on which you want to operate: "))
 
 
-# In[ ]:
+# In[3]:
 
 
-#Printing Values of Variables 
-print ("Title:" ,Title)
-print ("Artist:", Artist)
-print ("Album:", Album)
-print ("Recorded in:", Recording_Year)
-print ("Genre:", Genre)
-print ("Length:", Length, "m")
-print ("Track Number:", Track_Number)
-print ("Bitrate:", Bitrate_Value, Bitrate_Unit)
+# Check is file exists
+
+import os.path
+from os import path
+
+fileExists = path.exists(fileName)
+
+if (fileExists):
+    print (fileName + " file exists!")
+else:
+    fileObject = open(fileName, "w")
+    print("File " + fileName + " doesn't exist. No worries, a file of the same name is now created.")
+    newString = str(input("Please enter the text you want to write to this new file: "))
+    fileObject.write(newString)
+    fileObject.close()
+    exit()
+
+
+# In[4]:
+
+
+# Defining various functions for each file operation
+
+# toJustRead for simply reading the file contents
+def toJustRead():
+    print("Thank you. You chose to simply read the file. The file contents are:\n")
+    tempFile = open(fileName)
+    for line in tempFile:
+        print(line, end = "")
+
+# toDelete for simply deleting the contents of the file        
+def toDelete():
+    print('Thank you. You chose to delete all contents of the file.')
+    tempFile = open(fileName, "w")
+    print('All contents of the file have been deleted!')
+
+# toAppend for adding new content to the file
+def toAppend():
+    print('Thank you. You wish to add a new line to the file.')
+    newLine = str(input('Please enter the new content: '))
+    tempFile = open(fileName, "a")
+    tempFile.write(newLine + "\n")
+    print('The new content has been added to the file!')
+
+# toReplace for replacing a specified line with a new line
+def toReplace():
+    print('Thank you. You wish to replace a specific line with another one.')
+    lineNumber = int(input('Which line do you want to replace? '))
+    newContent = str(input('Please enter the content that should replace line number ' + str(lineNumber) + ': ' ))
+    tempFile = open(fileName, "r+")
+    counter = 1;
+    newFileContent = ""
+    for line in tempFile:
+        if (counter == lineNumber):
+            newFileContent = newFileContent + newContent + "\n"
+        else: 
+            newFileContent = newFileContent + line
+        counter += 1
+    tempFile.close()
+    tempFile = open(fileName, "w")
+    tempFile.write(newFileContent)
+    print("Line number " + str(lineNumber) + " has been replaced!")
+
+
+# In[5]:
+
+
+# initializing a {number : function name} dictionary to make it easy 
+# to replicate the 'switch' functionality which isn't avilable in Python
+# to call the desired funciton without getting into an if-else loop
+
+operations = {
+                1 : toJustRead, 
+                2 : toDelete, 
+                3 : toAppend, 
+                4 : toReplace,
+                5 : exit
+}
+
+
+# In[6]:
+
+
+# Presenting choices to the user ...
+
+print("Please enter a number for what you wish to do with the file ...")
+print("1: Read the file")
+print("2: Delete all contents of the file")
+print("3: Append to the existing contents in the file")
+print("4: Replace a specific line with a new line")
+print("5: Exit!")
+
+# Receiving user's input ...
+whatOperation = int(input ("Enter choice: "))
+
+# Calling appropriate function based on user's input ...
+operations[whatOperation]()
 
